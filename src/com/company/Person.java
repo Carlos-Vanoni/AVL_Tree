60eb3b8a1e786154373d57abbb3259191a71b6a2
edubernardi <77065000+edubernardi@users.noreply.github.com>
2022-06-14 20:00:55 -0300

    searchName implementation

---
+++ src/com/company/Person.java
@@ -0,0 +1,70 @@
+package com.company;
+import java.time.LocalDate;
+
+public class Person {
+
+    private String cpf;
+    private String rg;
+    private String name;
+    private LocalDate bornDate;
+    private String city;
+
+    public Person(String cpf, String rg, String name, LocalDate bornDate, String city) {
+        this.cpf = cpf;
+        this.rg = rg;
+        this.name = name;
+        this.bornDate = bornDate;
+        this.city = city;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(LocalDate bornDate) {
+        this.bornDate = bornDate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "cpf=" + cpf +
+                ", rg=" + rg +
+                ", name='" + name + '\'' +
+                ", bornDate=" + bornDate +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
---
+++ src/com/company/Reader.java
@@ -0,0 +1,78 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+
+    private Tree treeCpf;
+    private Tree treeName;
+    private Tree treeDate;
+
+
+    public Reader() {
+        this.treeCpf = new Tree();
+        this.treeName = new Tree();
+        this.treeDate = new Tree();
+    }
+
+    public void read(String user, String nome) {
+        try {
+            FileReader fis = new FileReader("C:\\Users\\" + user + "\\" + nome);
+            BufferedReader r = new BufferedReader(fis);
+
+            String line = "";
+            line = r.readLine();
+            ArrayList<Person> list = new ArrayList<>();
+            ArrayList<Person> listAux = new ArrayList<>();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+
+            while (line != null) {
+                String[] listSplited = line.split(";");
+
+
+                    String cpf = listSplited[0];
+                    String rg = listSplited[1];
+                    String name = listSplited[2];
+                    LocalDate bornDate = LocalDate.parse(listSplited[3], formatter);
+                    String city = listSplited[4];
+
+                    Person newPerson = new Person(cpf, rg, name, bornDate, city);
+
+                    treeCpf.insert(cpf, newPerson);
+                    treeDate.insert(bornDate.toString(), newPerson);
+                    treeName.insert(name, newPerson);
+
+                    System.out.println(cpf);
+                    System.out.println(rg);
+                    System.out.println(name);
+                    System.out.println(bornDate);
+                    System.out.println(city);
+                    System.out.println(" ");
+                    line = r.readLine();
+            }
+
+            r.close();
+        }catch (FileNotFoundException ex) {
+            System.out.println("Arquivo Nao Encontrado");
+        }catch (IOException ex) {
+            System.out.println("IOE");
+        }
+
+    }
+
+    public Person searchCPF(String cpf){
+        return treeCpf.searchPerson(cpf);
+    }
+
+    public ArrayList<Person>  searchName(String name){
+        return treeName.searchName(name);
+    }
+
+}
