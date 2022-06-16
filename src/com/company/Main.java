84dbe8e27739d1ae38c5150fdb4a2fdb1973b8b1
edubernardi <77065000+edubernardi@users.noreply.github.com>
2022-06-16 14:37:27 -0300

    Error handling

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,4 @@
+//Participantes: Eduardo Bernardi e Carlos Eduardo Vanoni
 package com.company;
 
 import java.util.ArrayList;
@@ -9,7 +10,9 @@ public class Main {
 
         ArrayList<Person> people;
 
-        while (true) {
+        boolean running = true;
+
+        while (running) {
             Reader reader = new Reader();
             UserInteraction userCommands = new UserInteraction(reader);
             String user = userCommands.leString("\n Digite usuário do seu computador: ");
@@ -21,6 +24,7 @@ public class Main {
                 String command = userCommands.leString("\nDigite 1 para pesquisar por uma pessoa pelo CPF: "
                         + "\nDigite 2 para pesquisar pessoas pelo nome"
                         + "\nDigite 3 para pesquisar pessoas que nasceram em certo periodo"
+                        + "\nDigite 4 para sair"
                         + "\nDigite 0 para voltar a seleção do arquivo");
 
                 if(command.equals("1")) {
@@ -34,7 +38,7 @@ public class Main {
                 if(command.equals("2")) {
                     String name = userCommands.leString("\n Digite o nome (ou iniciais): ");
                     people = userCommands.searchName(name);
-                    if (people.isEmpty())
+                    if (people == null || people.isEmpty())
                         System.out.println("Nao foi encontrado ninguem");
                     else
                         System.out.println(people);
@@ -43,10 +47,14 @@ public class Main {
                     String start = userCommands.leString("\n Digite a data inicial no formato DIA/MES/ANO: ");
                     String end = userCommands.leString("\n Digite a data final no formato DIA/MES/ANO: ");
                     people = userCommands.searchDate(start, end);
-                    if (people.isEmpty())
+                    if (people == null || people.isEmpty())
                         System.out.println("Nao foi encontrado ninguem");
                     else
                         System.out.println(people);
+                }
+                if(command.equals("4")) {
+                    running = false;
+                    break;
                 }
                 if(command.equals("0")) {
                     break;
